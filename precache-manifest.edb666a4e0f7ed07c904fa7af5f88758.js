8b24deadd075a328034062eeb107324b2cdfdfcb
prasannabalaji7 <prasannabalaji07@gmail.com>
2020-08-29 22:27:22 +0800

    Updates

---
+++ precache-manifest.edb666a4e0f7ed07c904fa7af5f88758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c93881c189ef4e730db8fb9719972a",
+    "url": "/shoping-cart/index.html"
+  },
+  {
+    "revision": "125b513e95ce2426cd93",
+    "url": "/shoping-cart/static/css/main.d064c02a.chunk.css"
+  },
+  {
+    "revision": "2df5d279912b3c72c966",
+    "url": "/shoping-cart/static/js/2.c534cb36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoping-cart/static/js/2.c534cb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125b513e95ce2426cd93",
+    "url": "/shoping-cart/static/js/main.0a9406b6.chunk.js"
+  },
+  {
+    "revision": "8c04ad60b9a924cf0676",
+    "url": "/shoping-cart/static/js/runtime-main.8d8acb35.js"
+  }
+]);
